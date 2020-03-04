b54d61af4ba3295bb6c67659fcadc62f7f5af5e6
Lawrence <yoonlawrence@yahoo.com>
2020-03-04 00:37:20 -0800

    Added css to SongList and App

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongList = () => {
+const SongList = (props) => {
+  console.log(props.songs);
+
+  // map over list, return
+  function renderList() {
+    return props.songs.map((song) => { // set key equal to title, because it's unique enough.. I tried to do i++ in todolist..
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    });
+  };
+
   return (
-    <div className="SongList">
-      SongList
+    <div className="SongList ui divided list">
+      {renderList()}
     </div>
   );
 };
 
 const mapStateToProps = (state) => { // state gets ALL state data from redux store
-  console.log(state);
-  return state;
+  return ({ songs: state.songs });
 };
 
 export default connect(mapStateToProps)(SongList);
